0aaac054cea208874d18870889eede3f02dde0ee
adii <muhamadbadri23@gmail.com>
2020-05-28 19:33:08 +0700

    Updates

---
+++ precache-manifest.3f1f5cec0f7f8b4b7277002661d90c93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1346f42166e929b389b03b0d917d9a",
+    "url": "/react-router-temp/index.html"
+  },
+  {
+    "revision": "942d840e95bd0b028614",
+    "url": "/react-router-temp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "771b702e1065abef1ab5",
+    "url": "/react-router-temp/static/css/main.c9e397e3.chunk.css"
+  },
+  {
+    "revision": "942d840e95bd0b028614",
+    "url": "/react-router-temp/static/js/2.f5e06c0c.chunk.js"
+  },
+  {
+    "revision": "a52153bcc847b73a84a3dde72b2b9c2e",
+    "url": "/react-router-temp/static/js/2.f5e06c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "771b702e1065abef1ab5",
+    "url": "/react-router-temp/static/js/main.5a16dee6.chunk.js"
+  },
+  {
+    "revision": "0d9e7161c9a70e926b0a",
+    "url": "/react-router-temp/static/js/runtime-main.a270e38e.js"
+  },
+  {
+    "revision": "9df716ac692c8cb3970d5507cdfa343b",
+    "url": "/react-router-temp/static/media/sun.9df716ac.svg"
+  }
+]);
